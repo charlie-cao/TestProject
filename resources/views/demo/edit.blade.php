06e277afb0124097c3cde893b2e747cb578e3b8e
qgame1914 <964870916@qq.com>
2017-11-06 17:03:38 +0800

    增加了mackdown的支持

--- resources/views/demo/edit.blade.php
+++ resources/views/demo/edit.blade.php
@@ -1,13 +1,31 @@
 @extends('demo.app')
 
 @section('content')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.1/showdown.min.js"></script>
+    <style>
+        blockquote {
+            border-left: #eee solid 5px;
+            padding-left: 20px;
+        }
+
+        ul li {
+            line-height: 20px;
+        }
+
+        code {
+            color: #D34B62;
+            background: #F6F6F6;
+        }
+
+    </style>
+
     <div class="container">
         <div class="row">
 
 
             <div class="col-sm-6">
                 <form>
-                    <div class="row"> < 返回 </div>
+                    <div class="row"> < 返回</div>
 
                     <div class="form-group">
                         <label for="title">标题</label>
@@ -15,7 +33,8 @@
                     </div>
                     <div class="form-group">
                         <label for="content">内容 文本／MD</label>
-                        <textarea class="form-control" rows="20"></textarea>
+                        <textarea id="oriContent" class="form-control" rows="10" onkeyup="convert()"></textarea>
+                        <div id="result"></div>
                     </div>
 
                 </form>
@@ -38,4 +57,13 @@
             </div>
         </div>
     </div>
+    <script>
+        function convert() {
+            var text = document.getElementById("oriContent").value;
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(text);
+            document.getElementById("result").innerHTML = html;
+        }
+
+    </script>
 @stop
--- resources/views/demo/home.blade.php
+++ resources/views/demo/home.blade.php
@@ -93,6 +93,7 @@
 
     <script src="https://unpkg.com/vue/dist/vue.js"></script>
     <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+
     <script>
         $(function () {
             $(".comment").hide();
